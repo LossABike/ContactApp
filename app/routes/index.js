e1bb235d43f4a541f4f9ddb2f42c8437592ff1cc
AzureX <ngoduchieuxxx@gmail.com>
2022-02-24 08:44:27 +0700

    update function handler error

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,13 @@ const routes = require('./contact.routes');
 function route(app) {
    
     app.use("/api/contacts", routes);
+    
+    app.use((err, req, res, next) => {
+        // Middleware xử lý lỗi tập trung.
+        // Trong các đoạn code xử lý ở các route, gọi next(error)
+        // sẽ chuyển về middleware xử lý lỗi này
+        errorHandler.handleError(error, res);
+        });
 }
 
 module.exports = route; 
